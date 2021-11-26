f693154ca102927059b2d805e9e8e1f87523b87d
Andreas Notokuusmo <andreasnotokusumo23@gmail.com>
2021-11-26 15:41:32 +0700

    :sparkles: React Helmet and Favicon Configuration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import { Helmet } from "react-helmet";
 import React, { useState, useCallback } from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
+import Auth from "./user/pages/Auth";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import Landing from "./shared/pages/Landing/Landing";
 import UpdatePlace from "./places/pages/UpdatePlace";
-import NavigationBar from "./shared/components/Navigation/NavigationBar";
-import Auth from "./user/pages/Auth";
 import { AuthContext } from "./shared/context/AuthContext";
+import NavigationBar from "./shared/components/Navigation/NavigationBar";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -27,15 +28,31 @@ function App() {
     routes = (
       <Switch>
         <Route exact path="/">
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>ShareLoct - Home</title>
+          </Helmet>
           <Users />
         </Route>
         <Route path="/:userId/places">
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>ShareLoct</title>
+          </Helmet>
           <UserPlaces />
         </Route>
         <Route path="/places/new">
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>ShareLoct - New Place</title>
+          </Helmet>
           <NewPlace />
         </Route>
         <Route path="/places/:placeId">
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>ShareLoct - Update Place</title>
+          </Helmet>
           <UpdatePlace />
         </Route>
         <Redirect to="/" />
@@ -45,12 +62,24 @@ function App() {
     routes = (
       <Switch>
         <Route exact path="/">
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>ShareLoct - Home</title>
+          </Helmet>
           <Users />
         </Route>
         <Route path="/:userId/places">
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>ShareLoct</title>
+          </Helmet>
           <UserPlaces />
         </Route>
         <Route path="/auth">
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>ShareLoct - Auth</title>
+          </Helmet>
           <Auth />
         </Route>
         <Redirect to="/auth" />
